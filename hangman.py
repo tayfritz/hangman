451db7380ee039f5f40f8901200f388fcea1eff5
Taylor Fritz <tayfritz01@gmail.com>
2020-09-30 17:17:22 -0400

    'Quit' setting added for the user to break out of the game at anytime

--- hangman.py
+++ hangman.py
@@ -13,14 +13,24 @@ words = [
 ]
 
 word_to_guess = words[random.randint(0, len(words))]
-max_guesses = len(word_to_guess)/2
-
-    
+max_guesses = len(word_to_guess)/round(2)
 
 print("Let's play a game of hangman! ")
 print("You have {} guesses to guess all the letter(s) in the word I am thinking of...".format((max_guesses)))
+user_status = []
 
-user_guess = input("Type a letter to start the game! What's your guess?   ")
+while max_guesses > 0:
+    user_guess = input("Type a letter to play the letter or type 'QUIT' to quit the game   ")
+
+    if user_guess.upper() == "QUIT":
+        break
+    elif user_guess not in word_to_guess:
+        print("Sorry, {} is not in the word I am thinking of.")
+        max_guesses -= 1
+        print("You now have {} guesses left.")
+    else:
+        user.status.append(user_guess)
+
 
 
 
