a2850655cb1bb115d470e397cf5eaa24688b0dc3
Taylor Fritz <tayfritz01@gmail.com>
2020-09-30 16:47:59 -0400

    Random word picker added to game

--- hangman.py
+++ hangman.py
@@ -1,3 +1,5 @@
+import random
+
 words = [
     "baseball", 
     "basketball", 
@@ -9,6 +11,15 @@ words = [
     "volleyball",
     "cheerleadering",
 ]
+
+word_to_guess = words[random.randint(0, len(words))]
+max_guesses = len(word_to_guess)/2
+print(max_guesses)
+
+
+for letters in word_to_guess:
+    print(letters)
+
 
 print("Let's play a game of hangman! ")
 user_guess = input("Type a letter to start the gane! What's your first guess?   ")
