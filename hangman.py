bd6cc57f49893a977fe40aad16bce0429df3bb6a
Taylor Fritz <tayfritz01@gmail.com>
2020-09-30 17:40:40 -0400

    Conditional arguments added when max_guesses are out

--- hangman.py
+++ hangman.py
@@ -13,13 +13,14 @@ words = [
 ]
 
 word_to_guess = words[random.randint(0, len(words))]
-max_guesses = len(word_to_guess)/round(2)
+max_guesses = len(word_to_guess)/2
+max_guesses = round(max_guesses)
 
 print("Let's play a game of hangman! ")
 print("You have {} guesses to guess all the letter(s) in the word I am thinking of...".format((max_guesses)))
 user_status = []
 
-while max_guesses > 0:
+while max_guesses != 0:
     user_guess = input("Type a letter to play or type 'quit' to quit the game   ")
 
     if user_guess == "quit":
@@ -31,7 +32,8 @@ while max_guesses > 0:
         max_guesses -= 1
         print("You now have {} guesses left.".format(max_guesses))
 
-
-
-
+if max_guesses == 0:
+    print("Sorry, you ran out of guesses. The word I was thinking about was {}".format(word_to_guess))
+    print("Here were your guesses.")
+    print(user_status)
 
